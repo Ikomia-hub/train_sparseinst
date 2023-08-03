c63965e9a3a144d7e7925a2d775b5f537017996c
ambroise <ambroise.berthe@ikomia.com>
2023-08-03 16:21:49 +0200

    Fix test file

--- train_sparseinst_test.py
+++ train_sparseinst_test.py
@@ -1,5 +1,4 @@
 import logging
-from ikomia.core import task, ParamMap
 from ikomia.utils.tests import run_for_test
 
 logger = logging.getLogger(__name__)
@@ -8,11 +7,11 @@ logger = logging.getLogger(__name__)
 def test(t, data_dict):
     logger.info("===== Test::train sparseinst =====")
     input_dataset = t.get_input(0)
-    params = task.get_parameters(t)
+    params = t.get_param_object(t)
     params["max_iter"] = 10
     params["batch_size"] = 1
     params["eval_period"] = 5
     params["split"] = 0.5
-    task.set_parameters(t, params)
+    t.set_parameters(params)
     input_dataset.load(data_dict["datasets"]["instance_segmentation"]["dataset_wgisd"])
     yield run_for_test(t)
