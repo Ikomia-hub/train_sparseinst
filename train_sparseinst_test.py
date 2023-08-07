730d1e55815c4b74e35aaf2dfb2a1164afc23474
ambroise <ambroise.berthe@ikomia.com>
2023-08-07 16:09:57 +0200

    Fix test file

--- train_sparseinst_test.py
+++ train_sparseinst_test.py
@@ -7,11 +7,11 @@ logger = logging.getLogger(__name__)
 def test(t, data_dict):
     logger.info("===== Test::train sparseinst =====")
     input_dataset = t.get_input(0)
-    params = t.get_param_object(t)
-    params["max_iter"] = 10
-    params["batch_size"] = 1
-    params["eval_period"] = 5
-    params["split"] = 0.5
+    params = t.get_parameters()
+    params["max_iter"] = "10"
+    params["batch_size"] = "1"
+    params["eval_period"] = "5"
+    params["split"] = "0.5"
     t.set_parameters(params)
-    input_dataset.load(data_dict["datasets"]["instance_segmentation"]["dataset_wgisd"])
+    input_dataset.load(data_dict["datasets"]["instance_segmentation"]["dataset_coco"])
     yield run_for_test(t)
